83b01fcaa052a99a2820daba4ace69a1ffb55caa
awaken <abdurashidov950@gmail.com>
2024-01-17 12:38:34 +0500

    Version 2

--- sanoq.py
+++ sanoq.py
@@ -1,16 +1,22 @@
-def decimal_to_binary(decimal_number):
+def decimal_to_base_n(decimal_number, base):
+    if not 2 <= base <= 36:
+        return "Base out of range (2-36)"
+    
     if decimal_number == 0:
         return '0'
 
-    binary = ''
-    while decimal_number > 0:
-        binary = str(decimal_number % 2) + binary
-        decimal_number //= 2
+    digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+    converted_number = ''
 
-    return binary
+    while decimal_number > 0:
+        remainder = decimal_number % base
+        converted_number = digits[remainder] + converted_number
+        decimal_number //= base
+
+    return converted_number
 
 decimal_number = 123  
+desired_base = 8       
 
-binary_number = decimal_to_binary(decimal_number)
-
-print(f"10 lik sanoq sistemasidagi  {decimal_number} son 2 lik sanoq sistemasida  {binary_number} ga teng")
+result = decimal_to_base_n(decimal_number, desired_base)
+print(f"{decimal_number} soninig {desired_base} sanoq sistemasidagi natijasi {result}")
